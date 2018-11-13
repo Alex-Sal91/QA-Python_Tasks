6b0bce5f5a13cda28e29565ff6e739c857e0f457
Alex-Sal91 <alexandersalako@outlook.com>
2018-11-13 22:27:07 +0000

    list comprehension way

--- Basic/Recursion-lists.py
+++ Basic/Recursion-lists.py
@@ -1,11 +1,7 @@
-list = []
-for i in range(1, 10):
-        list.append(i)
-        print(list)
-list_2 = []
-for j in list:
-        list_2.append(j*2)
-        print(list_2)
+list = [i for i in range(1, 10)]
+print(list)
+list_2 = [j * 2 for j in list]
+print(list_2)
 
 
 
