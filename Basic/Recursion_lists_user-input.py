e2f65c1a24299b0f9f1c9ca83a2d33e420e62d3f
Alex-Sal91 <alexandersalako@outlook.com>
2018-11-13 22:11:30 +0000

    Recursion with lists - takes in user input (list comprehension)

--- Basic/Recursion_lists_user-input.py
+++ Basic/Recursion_lists_user-input.py
@@ -1,11 +1,8 @@
 
 n = int(raw_input("Enter a number: "))
 n2 = int(raw_input("Enter another number: "))
-list = []
-for i in range(n, n2):
-        list.append(i)
-        print(list)
-list_2 = []
-for j in list:
-        list_2.append(j*2)
-        print(list_2)
+
+list = [num for num in range(n, n2)]
+print(list)
+list_2 = [num*2 for num in list]
+print(list_2)
